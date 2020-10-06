9c3ddd378ce63425c6df8d92e2975251aa756cfb
ChenxiiCheng <chenxic1011@gmail.com>
2020-10-05 21:53:55 -0400

    Updates

---
+++ precache-manifest.0969c6d29f71e15461527b7a8621fd27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c543991da96bdc0359efdc2f1a7583",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bf6d32c9ab249e32bdbd",
+    "url": "./static/js/2.9faca221.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.9faca221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "726c35dc06e952a7e44d",
+    "url": "./static/js/main.7fd32157.chunk.js"
+  },
+  {
+    "revision": "2217fcd00ffd726c3b1c",
+    "url": "./static/js/runtime-main.96adb595.js"
+  }
+]);
